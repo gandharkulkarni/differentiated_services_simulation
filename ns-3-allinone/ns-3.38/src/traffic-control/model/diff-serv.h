022b3d5c9ee9214e581b63d1483bd70a9134e438
gakulkarni <gakulkarni@dons.usfca.edu>
2023-05-07 23:18:09 -0700

    Updated logic for diffserv

--- ns-3-allinone/ns-3.38/src/traffic-control/model/diff-serv.h
+++ ns-3-allinone/ns-3.38/src/traffic-control/model/diff-serv.h
@@ -1,77 +1,41 @@
 #ifndef DIFFSERV_H
 #define DIFFSERV_H
-
 #include "traffic-class.h"
 
-#include "ns3/log.h"
-#include "ns3/packet.h"
 #include "ns3/queue.h"
 
-#include <queue>
 #include <vector>
 
-namespace ns3
-{
-
-enum QueueMode
-{
-    QUEUE_MODE_PACKETS,
-    QUEUE_MODE_BYTES,
-};
-
 using namespace ns3;
-using namespace std;
-// class TraceContainer;
 
 template <typename Packet>
-class DiffServ : public Queue<ns3::Packet>
+class DiffServ : public Queue<Packet>
 {
+  protected:
+    int in;
+    int out;
+    std::vector<TrafficClass> q_class;
+    bool DoEnqueue(Ptr<Packet> p);
+    Ptr<Packet> DoDequeue();
+    Ptr<Packet> DoRemove();
+    Ptr<const Packet> DoPeek();
+
   public:
-    static TypeId GetTypeId(void);
-
     DiffServ();
-
-    virtual ~DiffServ();
-
-    void SetMode(QueueMode mode);
-
-    QueueMode GetMode(void);
-
-    Ptr<ns3::Packet> Schedule(void);
-
-    uint32_t Classify(Ptr<ns3::Packet> p);
-
-    // bool Enqueue(Ptr<Packet> item);
-
-    // Ptr<Packet> Dequeue();
-
-    // Ptr<Packet> Remove();
-
-    // Ptr<const Packet> Peek(void) const;
-
-  private:
-    QueueMode m_mode;
-    std::vector<TrafficClass*> q_class;
-
-    bool DoEnqueue(Ptr<ns3::Packet> p);
-
-    Ptr<ns3::Packet> DoDequeue();
-
-    Ptr<ns3::Packet> DoRemove();
-
-    Ptr<const ns3::Packet> DoPeek() const;
-
-    //  using Queue<Packet>::Head;
-    //  using Queue<Packet>::Tail;
-    // using Queue<Packet>::DoEnqueue;
-    // using Queue<Packet>::DoDequeue;
-    // using Queue<Packet>::DoRemove;
-    // using Queue<Packet>::DoPeek;
-
-    // NS_LOG_TEMPLATE_DECLARE;
+    DiffServ(uint32_t maxPackets,
+             uint32_t priority_level,
+             bool isDefault,
+             Ipv4Address destIpAddr,
+             Ipv4Mask destMask,
+             uint32_t destPortNum,
+             uint32_t protNum,
+             Ipv4Address sourIpAddr,
+             Ipv4Mask sourMask,
+             uint32_t sourPortNum);
+    ~DiffServ();
+    static TypeId GetTypeId(void);
+    Ptr<Packet> Schedule();
+    uint32_t Classify(Ptr<Packet> p);
 };
 
-extern template class DiffServ<Packet>;
-} // namespace ns3
-
-#endif /* DIFFSERV_H */
+#endif
